7c7ab84ec42093b25da39ae5cfc968d3d4425a15
Marat Abdurakhmanov <abdurakhmanov.marat1997@gmail.com>
2026-07-06 22:42:58 +0300

    feat: add user and film dto

---
+++ src/main/java/ru/yandex/practicum/filmorate/dto/create/user/UserDto.java
@@ -0,0 +1,23 @@
+package ru.yandex.practicum.filmorate.dto.create.user;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class UserDto {
+    @NotBlank(message = "Email не может быть пустым")
+    @Email(message = "Email должен содержать символ @ и быть корректным")
+    private String email;
+
+    @NotBlank(message = "Логин не может быть пустым")
+    @Pattern(regexp = "^\\S+$", message = "Логин не должен содержать пробелов")
+    private String login;
+
+    private String name;
+
+    @NotNull(message = "Дата рождения обязательна")
+    @PastOrPresent(message = "Дата рождения не может быть в будущем")
+    private LocalDate birthday;
+}
