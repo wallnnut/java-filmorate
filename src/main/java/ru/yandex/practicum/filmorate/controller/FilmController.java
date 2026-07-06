b268edd215030c417896fed0177d452810b3ff12
Marat Abdurakhmanov <abdurakhmanov.marat1997@gmail.com>
2026-07-06 22:44:14 +0300

    feat: add controllers

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,43 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.dto.create.film.FilmDto;
+import ru.yandex.practicum.filmorate.exception.ResourceNotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.store.Store;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/films")
+@SuppressWarnings("unused")
 public class FilmController {
+
+    Store<Film> filmStore = new Store<>();
+
+    @PostMapping
+    public Film add(@RequestBody @Valid FilmDto film) {
+        UUID id = UUID.randomUUID();
+        Film createdFilm = Film.builder().id(id).build();
+        BeanUtils.copyProperties(film, createdFilm);
+        filmStore.add(createdFilm);
+        return createdFilm;
+    }
+
+    @GetMapping
+    public List<Film> getFilms() {
+        return filmStore.getItems();
+    }
+
+    @PutMapping("/{id}")
+    public Film edit(@PathVariable UUID id, @RequestBody @Valid FilmDto film) {
+        Film findedFilm = filmStore.getItemById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Фильм с ID " + id + " не найден"));
+        BeanUtils.copyProperties(film, findedFilm);
+        return findedFilm;
+    }
+
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,47 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.dto.create.user.UserDto;
+import ru.yandex.practicum.filmorate.exception.ResourceNotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.store.Store;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/users")
+@SuppressWarnings("unused")
+public class UserController {
+    Store<User> userStore = new Store<>();
+
+    @PostMapping
+    public User add(@RequestBody @Valid UserDto newUser) {
+        UUID id = UUID.randomUUID();
+
+        User createdUser = User.builder()
+                               .id(id)
+                               .build();
+
+        BeanUtils.copyProperties(newUser, createdUser);
+        userStore.add(createdUser);
+        return createdUser;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userStore.getItems();
+    }
+
+    @PutMapping("/{id}")
+    public User edit(@PathVariable UUID id, @RequestBody @Valid UserDto user) {
+        User findedUser = userStore.getItemById(id)
+                                   .orElseThrow(() ->
+                                           new ResourceNotFoundException("Пользователь с ID " + id + " не найден"));
+
+        BeanUtils.copyProperties(user, findedUser);
+        return findedUser;
+    }
+}
