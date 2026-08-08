89ae739a6af02ebb7c5fa82f966450821f542236
wall <unknown@mail.ru>
2026-08-08 17:39:27 +0300

    add rows for dictionaries

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+MERGE INTO genre (genre_id, name) KEY (genre_id) VALUES
+    (1, 'Комедия'),
+    (2, 'Драма'),
+    (3, 'Мультфильм'),
+    (4, 'Триллер'),
+    (5, 'Документальный'),
+    (6, 'Боевик');
+
+MERGE INTO age_rating (age_rating_id, name) KEY (age_rating_id) VALUES
+    (1, 'G'),
+    (2, 'PG'),
+    (3, 'PG-13'),
+    (4, 'R'),
+    (5, 'NC-17');
