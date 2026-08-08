92159f01506a8235f4d106985f4de6ac4c704dc8
wall <unknown@mail.ru>
2026-08-08 18:56:06 +0300

    bind host

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FilmorateApplication {
     public static void main(String[] args) {
+        System.setProperty("h2.bindAddress", "127.0.0.1");
         SpringApplication.run(FilmorateApplication.class, args);
     }
 
