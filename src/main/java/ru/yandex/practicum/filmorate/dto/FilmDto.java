6d16e96ef72afcd4818f97c5bf1f7ac36d5a8123
wall <unknown@mail.ru>
2026-08-08 18:09:46 +0300

    update filmdto and userdto

--- src/main/java/ru/yandex/practicum/filmorate/dto/FilmDto.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/FilmDto.java
@@ -5,15 +5,19 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Id;
+import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.validators.ValidReleaseDate;
 
 import java.time.LocalDate;
-
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Data
 public class FilmDto {
     private Id id;
+
     @NotBlank(message = "Название не может быть пустым")
     private String name;
 
@@ -26,4 +30,9 @@ public class FilmDto {
 
     @Positive(message = "Продолжительность должна быть положительным числом")
     private Integer duration;
+
+    private Set<Genre> genres = new LinkedHashSet<>();
+
+    @NotNull(message = "Рейтинг MPA обязателен")
+    private Mpa mpa;
 }
